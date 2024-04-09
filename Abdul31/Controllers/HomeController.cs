53d5952cc27c26ad0265987e29b3a41b2f350b59
abdul <abdulanm@gmail.com>
2024-04-10 01:09:30 +0530

    Add project files.

---
+++ Abdul31/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using Abdul31.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Abdul31.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string Details(int id,string name)
+        {
+            return " Id is " + id + " Name is " + name; 
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
